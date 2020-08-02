a3baaa1e3d95e24796ac051f7eb6b72734e50190
Luigi.Robles <Luigi.robles0846@my.riohondo.edu>
2020-08-02 15:12:18 -0700

    Add project files.

---
+++ CIT125_Ch.13_Fig_13-11/CIT125_Ch.13_Fig_13-11.cpp
@@ -0,0 +1,30 @@
+// CIT125 Intro To C++ Luigi Robles
+// Ch.13 pg.472 Fig.13-11 Zip Code Program
+// Checks whether a ZIP code contains valid number of characters
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//declare and initialize variables
+	string zipCode = "";
+
+	cout << "Five-character ZIP code (-1 to end): ";
+	cin >> zipCode; //input for ZIP code
+	
+	while (zipCode != "-1")
+	{
+		if (zipCode.length() == 5) //checking ZIP code for 5 characters
+			cout << "-->Correct number of characters";
+		else
+			cout << "-->Incorrect number of characters"; //False path for anything below 5 characters
+		//end if
+		cout << endl << endl;
+
+		cout << "Five-characters ZIP code (-1 to end): ";
+		cin >> zipCode; //continue with loop if -1 is not entered
+	} //end while
+	return 0;
+} //end of main function
